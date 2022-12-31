42c94034689ab695070ce144f1c1b3e553f478db
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-12-31 08:06:00 -0300

    jst

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
-/*
-  Open/Closed Principle (OCP)
-  Entidades abertas para extensão, porém fechadas para modificação
-*/
 import { Messaging } from './services/messaging';
 import { Order } from './classes/order';
 import { Persistence } from './services/persistence';
@@ -12,15 +8,39 @@ import {
   NoDiscount,
   TenPercentDiscount,
 } from './classes/discount';
+import { EnterpriseCustomer, IndividualCustomer } from './classes/customer';
+import { MessagingProtocol } from './classes/interfaces/messaging-protocol';
 
 //const fiftyPercentDiscount = new FiftyPercentDiscount();
 //const tenPercentDiscount = new TenPercentDiscount();
 const noDiscount = new NoDiscount();
 
+// const individualCustomer = new IndividualCustomer(
+//   'Matheus',
+//   'Felipe',
+//   '111.111.111-11',
+// );
+
+class MessagingMock implements MessagingProtocol {
+  sendMessage(msg: string): void {
+    console.log('Mensagem enviada pelo Mock: ', msg);
+  }
+}
+
+const enterpriseCustomer = new EnterpriseCustomer(
+  'Matheus PJ',
+  '111.001-34234/3423',
+);
 const shoppingCart = new ShoppingCart(noDiscount);
 const persistence = new Persistence();
 const messaging = new Messaging();
-const order = new Order(shoppingCart, messaging, persistence);
+const messagingMock = new MessagingMock();
+const order = new Order(
+  shoppingCart,
+  messagingMock,
+  persistence,
+  enterpriseCustomer,
+);
 
 shoppingCart.addItem(new Product('Camiseta', 49.9));
 shoppingCart.addItem(new Product('Caderno', 9.94758));
--- src/srp/entities/product.ts
+++
@@ -1,5 +0,0 @@
-import { CartItem } from '../entities/interfaces/cart-item';
-
-export class Product implements CartItem {
-  constructor(public name: string, public price: number) {}
-}
