dc620528e59c6831357c5e768cdaa9c5832f6974
MatheusTrinca <matheusfelipetrinca@gmail.com>
2023-01-01 16:21:48 -0300

    discount

--- src/classes/discount.spec.ts
+++ src/classes/discount.spec.ts
@@ -8,5 +8,18 @@ import {
 const createSut = (className: new () => Discount): Discount => new className();
 
 describe('Discount', () => {
-  it('should have no discount', () => {});
+  it('should have no discount', () => {
+    const sut = createSut(NoDiscount);
+    expect(sut.calculate(10.99)).toBeCloseTo(10.99);
+  });
+
+  it('should apply 15% discount on price', () => {
+    const sut = createSut(FiftyPercentDiscount);
+    expect(sut.calculate(150)).toBeCloseTo(75);
+  });
+
+  it('should apply 10% discount on price', () => {
+    const sut = createSut(TenPercentDiscount);
+    expect(sut.calculate(100)).toBeCloseTo(90);
+  });
 });
