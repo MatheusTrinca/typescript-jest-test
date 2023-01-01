c2a084d55d21ae25c9b1750f354062885d5d05db
MatheusTrinca <matheusfelipetrinca@gmail.com>
2023-01-01 09:36:47 -0300

    persistence test

--- src/services/persistence.spec.ts
+++ src/services/persistence.spec.ts
@@ -1,5 +1,25 @@
 import { Persistence } from './persistence';
 
 describe('Persistence', () => {
-  it('should return undefined', () => {});
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // system under test
+    const sut = new Persistence();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('should call console.log once', () => {
+    const sut = new Persistence();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call console.log with "Pedido salvo com sucesso"', () => {
+    const sut = new Persistence();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso');
+  });
 });
